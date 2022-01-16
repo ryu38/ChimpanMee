9c89dc5bd0255bbd092b22da350ac2b9aaf467c4
久賀隆矢 <niceneetlife.ryu@gmail.com>
2022-01-16 17:24:47 +0900

    set base theme

---
+++ lib/Color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  const AppColors._();
+
+  static Color get white => const Color(0xffffffff);
+  static Color get backWhite => const Color(0xfff8f8f8);
+
+  static Color get black => const Color(0xff000000);
+  static Color get brightBlack => const Color(0xff3f3f3f);
+
+  static Color get coffee => const Color(0xff977966);
+  static Color get milkCoffee => const Color(0xffF7ECE4);
+
+  static Color get banana => const Color(0xffFFE400);
+  static Color get milkBanana => const Color(0xffF6F1D7);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chimpanmee/Color.dart';
 import 'package:chimpanmee/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +27,27 @@ class MyApp extends StatelessWidget {
         return supportedLocales.first;
       },
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        primaryColor: AppColors.coffee,
+        scaffoldBackgroundColor: AppColors.backWhite,
+        appBarTheme: AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: AppColors.backWhite,
+            statusBarBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+          backgroundColor: AppColors.backWhite,
+          foregroundColor: AppColors.brightBlack,
+          elevation: 0,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: AppColors.coffee,
+          elevation: 0,
+        ),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: AppColors.milkBanana,
+          elevation: 0,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -46,27 +68,44 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final l10n = L10n.of(context)!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Text(l10n.helloWorld),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.photo_camera),
-            label: l10n.navBarCamera,
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(top: 32),
+        padding: const EdgeInsets.all(8),
+        width: 80,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Theme.of(context).bottomAppBarTheme.color,
+        ),
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.photo_camera),
           ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.insert_photo),
-            label: l10n.navBarGallery,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 4),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.insert_photo),
+              ),
+              const SizedBox(width: 64),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.web),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.web),
-            label: l10n.navBarWeb,
-          ),
-        ],
+        ),
       ),
     );
   }
